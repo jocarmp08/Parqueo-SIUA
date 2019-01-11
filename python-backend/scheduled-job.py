2aff7acd5e742ec7daad20996ac68748c23a326b
José Montoya <jocarmp08@babas>
2019-01-11 00:01:20 -0600

    Se configura el proceso programado para escribir en BD

--- python-backend/scheduled-job.py
+++ python-backend/scheduled-job.py
@@ -1,7 +1,7 @@
 # Save parking entrances at midnight
 import json
 import time
-
+from datetime import datetime, timezone
 import requests
 import schedule as schedule
 
@@ -10,20 +10,32 @@ This script executes a scheduled service that sends a PUT to the database to sto
 the parking lot during the day.
 """
 
+LOOPBACK_ENDPOINT = "http://167.99.240.71:3000/api/entrances"
 IN_MEMORY_DATA_ENDPOINT = "http://127.0.0.1:4097/restart"
 
 
 def start():
-    schedule.every(1).minutes.do(send_to_db)
+    schedule.every().day.at("00:05").do(send_to_db)
     while True:
         schedule.run_pending()
-        time.sleep(60)
+        time.sleep(1)
 
 
 def send_to_db():
     with open("backup.json", 'r') as json_file:
-        data = json.load(json_file)
-        print(data['parkingEntrancesCounter'])
+        parking_entrances = json.load(json_file)['parkingEntrancesCounter']
+        date = datetime.now(timezone.utc).astimezone()
+        day = date.weekday()
+
+        # Prepare entrance object
+        entrance = {
+            "quantity": parking_entrances,
+            "date": date,
+            "day": day,
+        }
+
+        post = requests.post(url=LOOPBACK_ENDPOINT, data=entrance)
+        print(post)
 
         # confirmation = requests.put(url=IN_MEMORY_DATA_ENDPOINT)
         # print(confirmation)
