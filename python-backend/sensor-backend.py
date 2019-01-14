0e81914911741954fab796e7f3a027126e77aab5
José Montoya <jocarmp08@babas>
2019-01-13 20:47:37 -0600

    Se modifica el socket para que utilice el método select()

--- python-backend/sensor-backend.py
+++ python-backend/sensor-backend.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import socket
+import selectors
 import threading
 import requests
 import datetime
@@ -15,27 +16,44 @@ PORT = 4096
 
 IN_MEMORY_DATA_ENDPOINT = "http://127.0.0.1:4097/common"
 
+# Use select method
+selector = selectors.DefaultSelector()
+
 
 def start():
     # Create socket, bind it and set to listen
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind((HOST, PORT))
         sock.listen()
+        sock.setblocking(False)
+        selector.register(sock, selectors.EVENT_READ, data=None)
         # Wait forever
         while True:
             print("Waiting for connections in {}:{}...".format(HOST, PORT))
-            # Block socket and wait for incoming connection
-            connection, address = sock.accept()
-            connection.setblocking(0)
-            # Process connection
-            process_incoming_connection(connection, address)
+            # Wait for changes in FD
+            events = selector.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    accept_connection(key.fileobj)
+                else:
+                    process_incoming_connection(key, mask)
 
 
-def process_incoming_connection(connection, address):
-    with connection:
-        print("Connected by", address)
+def accept_connection(sock):
+    connection, address = sock.accept()
+    print("Connected by", address)
+    connection.setblocking(False)
+    data = types.SimpleNamespace(add=address, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    selectors.register(connection, events, data=data)
+
+
+def process_incoming_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask and selector.EVENT_READ:
         # Receive data
-        data = connection.recv(4096).decode("utf-8")
+        recv_data = sock.recv(1024).decode("utf-8")
 
         # Modify counters and system flags (using threads so the HW doesn't wait)
         # A vehicle entered
@@ -45,7 +63,8 @@ def process_incoming_connection(connection, address):
         elif 'out' in data:
             threading.Thread(target=modify_counter_by_event, args=['out']).start()
 
-        connection.close()
+        selector.unregister(sock)
+        sock.close()
 
 
 def modify_counter_by_event(event):
